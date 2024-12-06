f90ad2b5d4384f237c8fc0b6d246710aaae1f112
Mairim Acosta <190049028+Celeste2727@users.noreply.github.com>
2024-12-06 14:55:12 -0600

    agregue los campos al modelo de editorial

--- app/Models/Editorial.php
+++ app/Models/Editorial.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Editorial extends Model
 {
-    //
+    static protected $table = "editorial";
+
+    protected $fillable = [
+        'nombre',
+        'direccion',
+        'telefono',
+    ];
+
 }
