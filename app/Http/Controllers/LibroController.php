b12ddaedcdca4c1493bfd7bce8801621c65d9db7
Israel Martinez <190049477+IsraelMartinez007@users.noreply.github.com>
2024-12-06 15:02:46 -0600

    agregue el controlador en el web.php

--- app/Http/Controllers/LibroController.php
+++ app/Http/Controllers/LibroController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Libro;
 
 class LibroController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('libros');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LibroController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("/libros",function(){
-    return view("libros");
-});
+Route::get("/libros", [LibroController::class, "index"]);
