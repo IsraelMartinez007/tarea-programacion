13601b485c54221c368a52297294bb151904a74d
Israel Martinez <190049477+IsraelMartinez007@users.noreply.github.com>
2024-12-06 14:54:07 -0600

    agregue los campos de del modelo libro

--- app/Models/Libro.php
+++ app/Models/Libro.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Libro extends Model
 {
-    //
+    protected $table = "libros";
+
+    protected $fillable = [
+        'id_editorial',
+        'titulo',
+        'numeedicion',
+        'numejemplar',
+    ];
 }
